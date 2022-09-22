4db4f8602aed81a1a1231b5a29ad32b070628eb0
Bhavesh2268 <113604988+Bhavesh2268@users.noreply.github.com>
2022-09-22 18:41:09 +0530

    Added Armstrong Number

---
+++ ArmstrongNumber.java
@@ -0,0 +1,26 @@
+package com.interviews;
+
+public class ArmstrongNumber {
+
+	public static void main(String[] args) {
+		
+		int a = 371;
+		
+		int temp = a;
+		
+		int digit1, digit2, digit3;
+		digit1=a%10;
+		a = a/10;
+		digit2 = a%10;
+		a = a/10;
+		digit3 = a%10;
+		
+		if(digit1*digit1*digit1+digit2*digit2*digit2+digit3*digit3*digit3 == temp) {
+			System.out.println(temp+" is Armstrong number");
+		}else {
+			System.out.println(temp+" is Not Armstrong Number");
+		}
+		
+	}
+	
+}
